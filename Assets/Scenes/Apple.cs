a5507e678920c92446251e33cd07b094d402a09f
dddxxx <50501819+dazeo666@users.noreply.github.com>
2020-02-06 19:12:35 -0500

    ApplePicker

---
+++ Assets/Scenes/Apple.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Apple : MonoBehaviour
+{
+    public static float bottomY = -20f;                                  // a
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);                                      // b
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();  // b
+                                                                             // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed();                                       // c
+        }
+        // Get a reference to the ApplePicker component of Main Camera
+
+    }
+}
+
+
---
+++ Assets/Scenes/Basket.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text scoreGT;
+
+    void Start()
+    {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
+    }
+
+    void Update()
+    {
+        // Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    { //
+
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+            score = score + 100;
+            scoreGT.text = score.ToString();
+
+            if (score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
